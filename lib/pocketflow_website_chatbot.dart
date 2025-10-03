0b908d1f8cf44e3165329a66b4982214a9697c8f
limcheekin <limcheekin@vobject.com>
2025-10-03 16:42:35 +0800

    feat: add `very_good create dart_cli` code

---
+++ lib/pocketflow_website_chatbot.dart
@@ -0,0 +1,10 @@
+/// pocketflow_website_chatbot, A Very Good Project created by Very Good CLI.
+///
+/// ```sh
+/// # activate pocketflow_website_chatbot
+/// dart pub global activate pocketflow_website_chatbot
+///
+/// # see usage
+/// pocketflow_website_chatbot --help
+/// ```
+library;
